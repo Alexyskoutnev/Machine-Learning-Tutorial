0c2e8ca8ee3a8d2caa027c1d33f1225b5359faad
alexyskoutnev <alexyskoutnev@gmail.com>
2023-10-13 09:51:46 -0500

    implemented boost classifer but it doesn't work

--- algorithms/gradient_boost.py
+++ algorithms/gradient_boost.py
@@ -1,7 +1,10 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.datasets import make_classification
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.tree import DecisionTreeClassifier
 import matplotlib.pyplot as plt
+
 
 def load_data(num_samples=100, seed=1):
     np.random.seed(seed)
@@ -10,9 +13,49 @@ def load_data(num_samples=100, seed=1):
     y = 2 * X + noise
     return X, y
 
+def load_data_class(num_samples=100, num_features=20, random_state=None):
+    """
+    Generate a synthetic classification dataset using make_classification.
+
+    Parameters:
+    - num_samples (int): The number of samples in the dataset.
+    - num_features (int): The number of features in the dataset.
+    - random_state (int or None): Random seed for reproducibility.
+
+    Returns:
+    - X (array-like): The feature matrix.
+    - y (array-like): The target labels.
+    """
+    X, y = make_classification(
+        n_samples=num_samples,
+        n_features=num_features,
+        random_state=random_state
+    )
+    return X, y
 
 class GradientBoostClassfier(object):
-    pass
+
+    def __init__(self, n_estimators=200, learning_rate=0.1, max_depth=3):
+        self.n_estimators = n_estimators
+        self.lr = learning_rate
+        self.max_depth = max_depth
+        self.estimators = []
+    
+    def fit(self, X, y):
+        predictions = np.zeros(len(y))
+        for _ in range(self.n_estimators):
+            residuals = y - 1 / (1 + np.exp(-predictions))
+            estimator = DecisionTreeClassifier(max_depth=self.max_depth)
+            estimator.fit(X, residuals)
+            prediction = estimator.predict(X)
+            predictions += self.lr * prediction
+            self.estimators.append(estimator)
+
+    def predict(self, X):
+        predictions = np.zeros(X.shape[0])
+        for estimator in self.estimators:
+            predictions += self.lr * estimator.predict(X)
+        return (1 / (1 + np.exp(-predictions))).round()
 
 class GradientBoostRegressor(object):
     """
@@ -98,7 +141,7 @@ class GradientBoostRegressor(object):
         plt.show()
 
 if __name__ == "__main__":
-    num_samples = 1000
+    num_samples = 100
     data = load_data(num_samples)
     X, y = data
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=292)
@@ -106,3 +149,7 @@ if __name__ == "__main__":
     boosted_tree_regressor.fit(X_train, y_train)
     y_pred = boosted_tree_regressor.predict(X_test)
     boosted_tree_regressor.plot(y_test, y_pred)
+    X_train_class, y_test_class = load_data_class(num_samples)
+    boosted_tree_classifer = GradientBoostClassfier()
+    # boosted_tree_classifer.fit(X_train_class, y_test_class)
+    # y_pred = boosted_tree_classifer.predict(X_train_class)
